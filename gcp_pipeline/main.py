764fd59e68f966ee49b4915181926dceca1cd159
oroconnor <oroconnor@gmail.com>
2026-06-01 15:25:58 -0400

    Fix dotenv path to load gcp_pipeline/.env correctly

--- gcp_pipeline/main.py
+++ gcp_pipeline/main.py
@@ -14,7 +14,7 @@ import sys
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from dotenv import load_dotenv
-load_dotenv()
+load_dotenv(os.path.join(os.path.dirname(os.path.abspath(__file__)), '.env'))
 
 import psycopg2
 from google_pricing_api import get_gemini_pricing
